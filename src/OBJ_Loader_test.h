2d64ccc1632e36775764a262b8e337001b6b631c
Paul Stahr <paul.stahr@gmx.de>
2021-10-03 18:53:43 +0200

    Added additional tests

--- src/OBJ_Loader_test.h
+++ src/OBJ_Loader_test.h
@@ -1,4 +1,5 @@
 #include "OBJ_Loader.h"
+#include "geometry.h"
 BOOST_AUTO_TEST_SUITE(OBJ_Loader)
 
 BOOST_AUTO_TEST_CASE( constructors_test )
@@ -11,6 +12,16 @@ BOOST_AUTO_TEST_CASE( data_access_test, * boost::unit_test::timeout(2))
     objl::Loader loader;
     loader.LoadFile("meshes/cube.obj");
     BOOST_CHECK_EQUAL( loader.LoadedMeshes.size(), 1 );
+    objl::Mesh const & m = loader.LoadedMeshes[0];
+    BOOST_CHECK_EQUAL( m.Vertices.size(), 14 );
+    BOOST_CHECK_EQUAL( m.Indices.size(), 36 );
+    uint8_t permutation[] = {5,1,0,4,2,6,3,7};
+    for (uint8_t i= 0; i < 8; ++i)
+    {
+        uint8_t idx = permutation[i];
+        vec3_t<objl::VertexCommon::pos_t> expected(idx & 1 ? -1 : 1, idx & 2 ? -1 : 1, idx & 4 ? -1 : 1);
+        BOOST_REQUIRE_MESSAGE(distQ(m.Vertices[i].Position,expected) < 1e-10, size_t(i) << "->" << size_t(idx) << " expected " << expected.x() << ',' << expected.y() << ',' << expected.z() << " got " << m.Vertices[i].Position.x() << ',' << m.Vertices[i].Position.y() << ',' << m.Vertices[i].Position.z());
+    }
 }
 
 BOOST_AUTO_TEST_SUITE_END()
--- src/io_util_test.h
+++ src/io_util_test.h
@@ -8,7 +8,7 @@ BOOST_AUTO_TEST_CASE( constructors_test )
 
 BOOST_AUTO_TEST_CASE( split_iterator_test )
 {
-    std::string str("foo batz bar");
+    std::string str("foo batz  bar 42");
     auto split_iter = IO_UTIL::make_split_iterator(str, [](char c){return c == ' ';});
     BOOST_CHECK_EQUAL(*split_iter, "foo" );
     ++split_iter;
@@ -16,7 +16,11 @@ BOOST_AUTO_TEST_CASE( split_iterator_test )
     ++split_iter;
     BOOST_CHECK_EQUAL(*split_iter, "bar" );
     ++split_iter;
-    BOOST_TEST(!split_iter.valid());    
+    int x = 0;
+    split_iter.parse(x);
+    BOOST_CHECK_EQUAL(x, 42);
+    ++split_iter;
+    BOOST_TEST(!split_iter.valid());   
 }
 
 BOOST_AUTO_TEST_SUITE_END()
