70f3294408695f0ad7bbdf93483b44e0965ca069
Paul Stahr <paul.stahr@gmx.de>
2022-03-22 11:20:20 +0100

    Added missing changes

--- src/gl_util.h
+++ src/gl_util.h
@@ -19,4 +19,39 @@ template <> struct gl_type_struct<int32_t>  {enum{value = GL_INT};};
 template <> struct gl_type_struct<float>    {enum{value = GL_FLOAT};};
 
 template <typename T>const GLint gl_type = gl_type_struct<T>::value;
+
+template <typename V, typename MakePointer>
+inline void gen_resources(size_t count, V output_iter, std::function<void(GLsizei, GLuint*)> allocator, std::function<void(GLuint)> deleter, MakePointer mp)
+{
+    while (count > 0)
+    {
+        std::array<GLuint, 32> tmp_id;
+        size_t blk = std::min(count, tmp_id.size());
+        allocator(blk, &tmp_id[0]);
+        for (size_t i = 0; i < blk; ++i)
+        {
+            *output_iter = mp(tmp_id[i],deleter);
+            ++output_iter;
+        }
+        count -= blk;
+    }
+}
+
+template <typename T, typename V>
+void gen_resources_shared(size_t count, V output_iter, std::function<void(GLsizei, GLuint*)> allocator, std::function<void(GLuint)> deleter)
+{
+    gen_resources(count, output_iter, allocator, deleter, [](auto id, auto deleter){return std::make_shared<T>(id,deleter);});
+}
+
+template <typename T, typename V>
+void gen_resources_unique(size_t count, V output_iter, std::function<void(GLsizei, GLuint*)> allocator, std::function<void(GLuint)> deleter)
+{
+    gen_resources(count, output_iter, allocator, deleter, [](auto id, auto deleter){return std::make_unique<T>(id,deleter);});
+}
+
+template <typename T, typename V>
+void gen_resources_direct(size_t count, V output_iter, std::function<void(GLsizei, GLuint*)> allocator, std::function<void(GLuint)> deleter)
+{
+    gen_resources(count, output_iter, allocator, deleter, [](auto id, auto deleter){return std::move(T(id,deleter));});
+}
 #endif
